ba6fd8ca574498d4415b20341e385cfcfd859290
MohamedJakkariya <66210702+MohamedJakkariya@users.noreply.github.com>
2020-10-09 22:19:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>This is my create create app with codesandbox</h2>
+    </div>
+  );
+}
